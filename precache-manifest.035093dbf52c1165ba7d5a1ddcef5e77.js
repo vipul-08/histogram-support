cc491eb43f6ed7f69a915ad9c06b8b7eb9dd1fba
vipul.s <vipul.s@media.net>
2020-07-29 12:21:02 +0530

    Updates

---
+++ precache-manifest.035093dbf52c1165ba7d5a1ddcef5e77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc3a8493fcdac50172acad2a5f7fd841",
+    "url": "/histogram-support/index.html"
+  },
+  {
+    "revision": "9d339eadd56ded956fdf",
+    "url": "/histogram-support/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1fc991298cf68d1c5a2f",
+    "url": "/histogram-support/static/js/2.96232835.chunk.js"
+  },
+  {
+    "revision": "3762f862101e9a33484d9e48ef4fedbc",
+    "url": "/histogram-support/static/js/2.96232835.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d339eadd56ded956fdf",
+    "url": "/histogram-support/static/js/main.df6d6ab5.chunk.js"
+  },
+  {
+    "revision": "e0d580c97fa414db4943",
+    "url": "/histogram-support/static/js/runtime-main.1ae0a419.js"
+  }
+]);
